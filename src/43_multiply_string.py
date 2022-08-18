6e57e7efc0bb12f922052784ff6912457e50b83b
abhinav059 <abhinavv0179@gmail.com>
2022-08-18 21:22:13 +0530

    function to multiply a string

---
+++ src/43_multiply_string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        x = int(num1)
+        y = int(num2)
+        res = x*y
+        res_str = str(res)
+        return res_str
+        
