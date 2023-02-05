0984ecfb23f6d46789c0dd734776c90baf3a4c62
Abhinav Kumar <abhinavv0179@gmail.com>
2023-02-05 17:22:40 +0530

    newquestion

---
+++ src/1089_Duplicate_Zeros.py
@@ -0,0 +1,10 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        i = 0
+        n = len(arr)
+        while i<n:
+            if arr[i] == 0:
+                arr.pop(-1)
+                arr.insert(i,0)
+                i+=1
+            i+=1
